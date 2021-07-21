dd0cf4d8be40cf2af39f38b1aafa82c826bf5663
wangjinjian <wangjinjian@sjtu.edu.cn>
2021-07-21 10:11:48 +0800

    0721

--- pythondoc.py
+++ pythondoc.py
@@ -1,44 +1,9 @@
 from docx import Document
 import os
 
-dic = {
-    '(1)': '(一)',
-    '(2)': '(二)',
-    '(3)': '(三)',
-    '(4)': '(四)',
-    '(5)': '(五)',
-    '(6)': '(六)',
-    '1)': '一)',
-    '2)': '二)',
-    '3)': '三)',
-    '4)': '四)',
-    '5)': '五)',
-    '6)': '六)',
-    '1、': '一、',
-    '2、': '二、',
-    '3、': '三、',
-    '4、': '四、',
-    '5、': '五、',
-    '1）': '一)',
-    '2）': '二)',
-    '3）': '三)',
-    '4）': '四)',
-    '5）': '五)',
-    '6）': '六)',
-    '1.': '一、',
-    '2.': '二、',
-    '3.': '三、',
-    '4.': '四、',
-    '5.': '五、',
-    '（1）': '(一)',
-    '（2）': '(二)',
-    '（3）': '(三)',
-    '（4）': '(四)',
-    '（5）': '(五)',
-    '（6）': '(六)',
-}
+
 dic1 = {'一、', '二、', '三、', '四、', '五、', '六、', '七、'}
-path = '/Users/wangjinjian/Desktop/23/技能等级评价--上海公司题库--笔试/供电服务员(抄表核算收费员)-抄表核算收费员/抄表核算收费员技能笔试题库.docx'
+path = '/Users/wangjinjian/Desktop/233/技能等级评价--上海公司题库--笔试/变配电运行值班员(变电站运行值班员)-电力调度员（主网）/地级调度--电力调度员（主网）技能笔试题库1.docx'
 doc = Document(path)
 cnt = 1
 for con in doc.paragraphs:
@@ -48,13 +13,6 @@ for con in doc.paragraphs:
             content = str(cnt) + '、' + content[10:]
             cnt += 1
             con.text = content
-        elif content[0:2] == '答：':
-            content = content[2:]
-            con.text = content
-        elif content[0:3] in dic.keys():
-            con.text = dic[content[0:3]] + content[3:]
-        elif content[0:2] in dic.keys():
-            con.text = dic[content[0:2]] + content[2:]
         elif content[:2] in dic1:
             con.text = ''
 paths = path.split('/')
