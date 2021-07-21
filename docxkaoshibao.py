5384f7f41b64fcd83fa298b6c8bad2969cb05c89
wangjinjian <wangjinjian@sjtu.edu.cn>
2021-07-21 09:21:05 +0800

    0721

--- docxkaoshibao.py
+++ docxkaoshibao.py
@@ -30,7 +30,7 @@ class docxx:
             if skiptimes != 0:
                 skiptimes -= 1
                 continue
-            if len(paras[i].text) != 0:
+            if len(paras[i].text.strip()) != 0:
                 if paras[i].text[:2] in dic:
                     if paras[i].text[2:] in self.ignore:
                         skip = True
@@ -51,7 +51,7 @@ class docxx:
                 elif paras[i].text[:2] == char2:
                     temp = paras[i].text
                     anindex = i
-                    while (anindex + 2 < palen and len(paras[anindex + 1].text) != 0):
+                    while (anindex + 2 < palen and len(paras[anindex + 1].text.strip()) != 0):
                         temp += ' ' * 5 + paras[anindex + 1].text
                         paras[anindex + 1].text = ''
                         anindex += 1
@@ -67,4 +67,6 @@ class docxx:
 if __name__ == '__main__':
     path1 = '/Users/wangjinjian/Desktop/23/技能等级评价--上海公司题库--笔试/供电服务员(抄表核算收费员)-抄表核算收费员/抄表核算收费员技能笔试题库.docx'
     path = '/Users/wangjinjian/Desktop/23/技能等级评价--上海公司题库--笔试/供电服务员(装表接电工)-装表接电工/装表接电工技能笔试题库.docx'
-    docxx(path).fun()
+    path2='/Users/wangjinjian/Desktop/233/技能等级评价--上海公司题库--笔试/供电服务员(装表接电工)-装表接电工/装表接电工技能笔试题库.docx'
+    path3='/Users/wangjinjian/Desktop/233/技能等级评价--上海公司题库--笔试/供电服务员(用电检查（稽查）员)-用电监察员/用电监察员技能笔试题库 2.docx'
+    docxx(path3).fun()
