bf0d79ffe62501de74f377bea348c39794463e6f
wangjinjian <wangjinjian@sjtu.edu.cn>
2022-11-15 16:32:06 +0800

    1115

--- ßà.py
+++ ßà.py
@@ -1,12 +1,12 @@
 from openpyxl import load_workbook, Workbook
 import os
 
-path = '/Users/wangjinjian/Desktop/深度学习补充题.xlsx'
+path = '/Users/wangjinjian/Desktop/关于开展公司2022年大数据应用技能竞赛模拟测试的通知/2022大数据应用技能竞赛参考样题..xlsx'
 dirname = os.path.dirname(path)
 filename = os.path.basename(path)
 wbnw = Workbook()
 wsnw = wbnw.active
-wsnw.append(['题型', '题目', 'A', 'B', 'C', 'D', 'E', 'F', '答案'])
+wsnw.append(['题型', '题目', 'A', 'B', 'C', 'D', '答案'])
 wb = load_workbook(path)
 ws = wb.active
 res = []
@@ -18,9 +18,13 @@ for i in range(2, ws.max_row + 1):
     da = ws.cell(row=i, column=5).value
     if tm == None or tx == None or xx == None or da == None:
         continue
-    arrs = [arr[1:].strip('.、 ') for arr in xx.split('\n')]
-    if len(arrs) < 6:
-        arrs.extend([''] * (6 - len(arrs)))
+    arrs = [arr.strip('.、 ').replace('$;$','') for arr in xx.split('\n')]
+    if len(arrs)<4:
+        arrs+=['']*(4-len(arrs))
+    if len(da)==1:
+        tx='单选题'
+    else:
+        tx='多选题'
     r.append(tx)
     r.append(tm)
     r.extend(arrs)
